4f6aa58242571c52d9302293c2ff51d141678b34
Dan Phillips <danmakenoise@gmail.com>
2017-10-22 15:28:57 -0400

    add lint exception for server

--- src/server/start.js
+++ src/server/start.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('listening on localhost:3000');
+  console.log('listening on localhost:3000'); // eslint-disable-line no-console
 });
